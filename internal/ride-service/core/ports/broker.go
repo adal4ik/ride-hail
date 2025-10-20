ad29c8bca9fc086ebbd5edea19c9102fc3990f68
arsen <arsen.serikbai01@gmail.com>
2025-10-20 18:25:07 +0500

    send create ride msg to broker

--- internal/ride-service/core/ports/broker.go
+++ internal/ride-service/core/ports/broker.go
@@ -1,5 +1,11 @@
 package ports
 
+import (
+	"context"
+	messagebrokerdto "ride-hail/internal/ride-service/core/domain/message_broker_dto"
+)
+
 type IRidesBroker interface {
-	
+	Close() error
+	PushMessage(ctx context.Context, message messagebrokerdto.Ride) error
 }
--- internal/ride-service/core/services/rides_service.go
+++ internal/ride-service/core/services/rides_service.go
@@ -2,7 +2,10 @@ package services
 
 import (
 	"context"
+	"fmt"
+	"ride-hail/internal/mylogger"
 	"ride-hail/internal/ride-service/core/domain/dto"
+	messagebrokerdto "ride-hail/internal/ride-service/core/domain/message_broker_dto"
 	"ride-hail/internal/ride-service/core/domain/model"
 	"ride-hail/internal/ride-service/core/ports"
 )
@@ -12,15 +15,18 @@ type RidesService struct {
 	RidesBroker    ports.IRidesBroker
 	RidesWebsocket ports.IRidesWebsocket
 	ctx            context.Context
+	mylog          mylogger.Logger
 }
 
 func NewRidesService(ctx context.Context,
+	mylog mylogger.Logger,
 	RidesRepo ports.IRidesRepo,
 	RidesBroker ports.IRidesBroker,
 	RidesWebsocket ports.IRidesWebsocket,
 ) ports.IRidesService {
 	return &RidesService{
-		ctx: ctx,
+		ctx:            ctx,
+		mylog:          mylog,
 		RidesRepo:      RidesRepo,
 		RidesBroker:    RidesBroker,
 		RidesWebsocket: RidesWebsocket,
@@ -28,14 +34,29 @@ func NewRidesService(ctx context.Context,
 }
 
 // implement me
-func (rs *RidesService) CreateRide(dto.RidesRequestDto) (dto.RidesResponseDto, error) {
+func (rs *RidesService) CreateRide(ride dto.RidesRequestDto) (dto.RidesResponseDto, error) {
+	mylog := rs.mylog.Action("create ride")
+
+	mylog.Info("Inserting ride to DB")
+
 	m := model.Rides{}
-	ctx, cancel  := context.WithCancel(rs.ctx)
+	ctx, cancel := context.WithCancel(rs.ctx)
 	defer cancel()
-	
+
 	_, err := rs.RidesRepo.CreateRide(ctx, m)
 	if err != nil {
 		return dto.RidesResponseDto{}, err
 	}
+
+	// publish message to rabbitmq
+	mylog.Info("Inserting ride to BM")
+
+	rideMsg := messagebrokerdto.Ride{}
+
+	if err := rs.RidesBroker.PushMessage(rs.ctx, rideMsg); err != nil {
+		mylog.Error("Failed to publish message", err)
+		return dto.RidesResponseDto{}, fmt.Errorf("cannot send message to broker: %w", err)
+	}
+
 	return dto.RidesResponseDto{}, nil
 }
