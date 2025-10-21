68f0aad83043f48eaf8e956e023cc52ab73a7868
yerlan <>
2025-10-21 11:49:33 +0500

    upd(migration): jwt deleted

--- migrations/000011_create_jwt_table.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS  jwt_tokens (
-    jwt_token_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
-    user_id UUID REFERENCES users(user_id) ON DELETE CASCADE,
-    refresh_token TEXT NOT NULL
-);
