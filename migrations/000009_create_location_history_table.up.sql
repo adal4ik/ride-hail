538f8e7eb5c57958e6f62886a0e68a441fe193b9
arsen <>
2025-10-24 17:10:52 +0500

    fix migrations

--- migrations/000009_create_location_history_table.up.sql
+++ migrations/000009_create_location_history_table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS location_history (
   location_history_id UUID PRIMARY KEY DEFAULT uuid_generate_v4 (),
-  coord_id UUID REFERENCES coordinates (coordinate_id),
+  coord_id UUID REFERENCES coordinates (coord_id),
   driver_id UUID REFERENCES drivers (driver_id),
   latitude DECIMAL(10, 8) NOT NULL CHECK (latitude BETWEEN -90 AND 90),
   longitude DECIMAL(13, 8) NOT NULL CHECK (longitude BETWEEN -180 AND 180),
