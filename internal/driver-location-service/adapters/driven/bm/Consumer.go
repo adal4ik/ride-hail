2a67289d2bae4cdf2e57f68f3997f3f900d18a5f
adal4ik <adilet.adilek@gmail.com>
2025-10-26 17:04:27 +0500

    feat: add listen ride sstatus

--- internal/driver-location-service/adapters/driven/bm/Consumer.go
+++ internal/driver-location-service/adapters/driven/bm/Consumer.go
@@ -4,63 +4,117 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"ride-hail/internal/driver-location-service/core/domain/dto"
-	ports "ride-hail/internal/driver-location-service/core/ports/driven"
-	"ride-hail/internal/mylogger"
 	"time"
+
+	"ride-hail/internal/driver-location-service/core/domain/dto"
+	"ride-hail/internal/driver-location-service/core/ports/driven"
+	"ride-hail/internal/mylogger"
+
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
-type IConsumer interface {
-	SubscribeForMessages() error
-}
+const (
+	bindRideRequest = "ride.request.*"
+	bindRideStatus  = "ride.status.*"
+)
 
 type Consumer struct {
-	ctx      context.Context
-	log      mylogger.Logger
-	broker   ports.IDriverBroker
-	messages chan dto.RideDetails
+	ctx    context.Context
+	log    mylogger.Logger
+	broker driven.IDriverBroker
+
+	RideRequests chan dto.RideDetails
+	RideStatuses chan dto.RideStatusUpdate
 }
 
-func NewConsumer(ctx context.Context, broker ports.IDriverBroker, log mylogger.Logger, messages chan dto.RideDetails) *Consumer {
+func NewConsumer(ctx context.Context, broker driven.IDriverBroker, log mylogger.Logger) *Consumer {
 	return &Consumer{
-		ctx:      ctx,
-		broker:   broker,
-		log:      log,
-		messages: messages,
+		ctx:          ctx,
+		broker:       broker,
+		log:          log,
+		RideRequests: make(chan dto.RideDetails, 64),
+		RideStatuses: make(chan dto.RideStatusUpdate, 64),
 	}
 }
 
-func (c *Consumer) SubscribeForMessages() error {
-	time.Sleep(2 * time.Second)
-	msgCh, err := c.broker.Consume(c.ctx, "ride_requests", "ride.request.{ride_type}", ports.ConsumeOptions{
-		Prefetch:     1,
-		AutoAck:      false,
-		QueueDurable: true,
-	})
+func (c *Consumer) ListenAll() error {
+	reqMsgs, err := c.broker.Consume(
+		c.ctx,
+		"ride_request_queue",
+		bindRideRequest,
+		driven.ConsumeOptions{Prefetch: 20, AutoAck: false, QueueDurable: true},
+	)
 	if err != nil {
-		c.log.Action("consume").Error("messages channel closed", nil)
-		return err
+		return fmt.Errorf("consume ride.request: %w", err)
 	}
-	go func() {
-		for msg := range msgCh {
-			var unmarshedMsg dto.RideDetails
-			fmt.Println("Received message in Consumer Messsage Content Type: ", msg.ContentType)
-			if err := json.Unmarshal(msg.Body, &unmarshedMsg); err != nil {
-				c.log.Action("consume").Error("failed to unmarshal message", err)
+
+	statusMsgs, err := c.broker.Consume(
+		c.ctx,
+		"ride_status_queue",
+		bindRideStatus,
+		driven.ConsumeOptions{Prefetch: 20, AutoAck: false, QueueDurable: true},
+	)
+	if err != nil {
+		return fmt.Errorf("consume ride.status: %w", err)
+	}
+
+	go c.listenRequests(reqMsgs)
+	go c.listenStatuses(statusMsgs)
+
+	c.log.Info("Consumers started for ride.request.* and ride.status.*")
+	return nil
+}
+
+func (c *Consumer) listenRequests(msgs <-chan amqp.Delivery) {
+	for {
+		select {
+		case <-c.ctx.Done():
+			c.log.Info("stop ride.request consumer: context done")
+			return
+		case msg, ok := <-msgs:
+			if !ok {
+				c.log.Info("ride.request channel closed")
+				return
+			}
+
+			var req dto.RideDetails
+			if err := json.Unmarshal(msg.Body, &req); err != nil {
+				c.log.Action("consume.request").Error("failed to unmarshal ride request", err)
+				_ = msg.Nack(false, false)
 				continue
 			}
-			fmt.Println("Unmarshalled message: ", unmarshedMsg)
-			c.messages <- unmarshedMsg
-			c.log.Action("consume").Info("message received", nil)
-			// Process the message
-			c.log.Action("consume").Info("message body: %s", string(msg.Body))
-			// Acknowledge the message
-			if err := msg.Ack(false); err != nil {
-				c.log.Action("consume").Error("failed to acknowledge message", err)
-			} else {
-				c.log.Action("consume").Info("message acknowledged", nil)
-			}
+
+			c.RideRequests <- req
+			_ = msg.Ack(false)
+			c.log.Action("consume.request").Info("received ride request",
+				"ride_id", req.RideID, "type", req.RideType)
 		}
-	}()
-	return nil
+	}
+}
+
+func (c *Consumer) listenStatuses(msgs <-chan amqp.Delivery) {
+	for {
+		select {
+		case <-c.ctx.Done():
+			c.log.Info("stop ride.status consumer: context done")
+			return
+		case msg, ok := <-msgs:
+			if !ok {
+				c.log.Info("ride.status channel closed")
+				return
+			}
+
+			var st dto.RideStatusUpdate
+			if err := json.Unmarshal(msg.Body, &st); err != nil {
+				c.log.Action("consume.status").Error("failed to unmarshal ride status", err)
+				_ = msg.Nack(false, false)
+				continue
+			}
+
+			c.RideStatuses <- st
+			_ = msg.Ack(false)
+			c.log.Action("consume.status").Info("received ride status",
+				"ride_id", st.RideID, "status", st.Status)
+		}
+	}
 }
